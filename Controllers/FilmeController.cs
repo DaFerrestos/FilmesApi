3da3dbd43bb9bc741b35cda8a70c4a28c7e5b246
DaFerrestos <dabla.ferrestos@gmail.com>
2022-03-17 22:28:51 -0300

    inclusão de classe, modelo, construtor e primeira requisição [frombody] pelo Postman

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,26 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController] //identifica tipo da classe: Controller
+    [Route("[controller]")] //Explicita rota
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+
+
+
+        public void AdicionaFilme([FromBody]Filme filme) {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        public int Duracao { get; set; }
+    }
+}
