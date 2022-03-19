1fce389d058705e3e282c98e10ee3c6782fa18de
DaFerrestos <dabla.ferrestos@gmail.com>
2022-03-19 16:01:14 -0300

    Introdução a recuperação de dados e melhores praticas

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -9,23 +9,33 @@ namespace FilmesApi.Controllers
 {
     [ApiController] //identifica tipo da classe: Controller
     [Route("[controller]")] //Explicita rota
-    public class FilmeController : ControllerBase
-    {
+    public class FilmeController : ControllerBase {
         private static List<Filme> filmes = new List<Filme>();
         private static int id = 1;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody]Filme filme) {
+        public IActionResult AdicionaFilme([FromBody] Filme filme) {
 
             filme.Id = id++;
             filmes.Add(filme);
+
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id}, filme);//indica local(location) de criação
         }
 
         [HttpGet]
-        public IEnumerable<Filme> RecuperarFilmesSalvos() {
-            return filmes;
+        public IActionResult RecuperaFilmes() {
+            return Ok(filmes);
         }
 
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id) {
 
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if(filme != null) {
+                return Ok(filme);
+            }
+            return NotFound();//devolve mensagem mais clara para o usuário
+        }
     }
 }
