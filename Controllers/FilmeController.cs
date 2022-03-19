fe1c4463e233ab6d1c9167efcc7b0ccb67635466
DaFerrestos <dabla.ferrestos@gmail.com>
2022-03-19 15:33:03 -0300

    incluindo ids para informações postadas

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -12,12 +12,20 @@ namespace FilmesApi.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
         public void AdicionaFilme([FromBody]Filme filme) {
-            
+
+            filme.Id = id++;
             filmes.Add(filme);
         }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmesSalvos() {
+            return filmes;
+        }
+
 
     }
 }
--- Models/Filme.cs
+++ Models/Filme.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace FilmesApi.Models
 {
     public class Filme
-    {   
+    {
+        public int Id { get; set; }
         [Required(ErrorMessage ="O campo título é obrigatório")]
         public string Titulo { get; set; }
         [Required(ErrorMessage ="O campo diretor é obrigatório")]
@@ -16,5 +17,6 @@ namespace FilmesApi.Models
         public string Genero { get; set; }
         [Range(1,800, ErrorMessage = "A duração deve ter entre 1 e 800 minutos")]
         public int Duracao { get; set; }
+       
     }
 }
